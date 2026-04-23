a3dbb91a134e6ccfec6385799ab5beec97ce267d
mokhtar <mokhtar@mokhtar-HP-Compaq-8000-Elite-SFF-PC>
2026-04-23 17:57:48 +0200

    initial design for inventory system

---
+++ src/main/java/com/mycompany/smartinventorymanagementsystem/SmartInventoryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smartinventorymanagementsystem;
+
+/**
+ *
+ * @author mokhtar
+ */
+public class SmartInventoryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
